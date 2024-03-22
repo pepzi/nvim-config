ba8dea37dd10994a8affccf1f0593f0f66564e66
Robert Sand <robert@pepzi.org>
2024-03-22 01:37:14 +0100

    add noice

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -86,6 +86,21 @@ local plugins = {
         opts or {})
     end
   },
+  {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+  },
 
   -- completion
   'hrsh7th/nvim-cmp',
