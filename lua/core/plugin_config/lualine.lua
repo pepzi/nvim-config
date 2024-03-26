fe395bc48b9faaeffdb0e098ec30d9a65715f0af
Robert Sand <robert@pepzi.org>
2024-03-26 04:20:17 +0100

    THIS is the commit that adds Codeium status in lualine

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,6 +1,83 @@
-require('lualine').setup {
+local function show_macro_recording()
+    local recording_register = vim.fn.reg_recording()
+    if recording_register == "" then
+        return ""
+    else
+        return "Recording @" .. recording_register
+    end
+end
+
+local function codeium_status()
+  return vim.api.nvim_call_function("codeium#GetStatusString", {})
+end
+
+local lualine = require('lualine')
+lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'catppuccin-mocha',
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
   },
+  sections = {
+    lualine_a = {'mode', codeium_status},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = { { "macro-recording", fmt = show_macro_recording }, 'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+    callback = function()
+        lualine.refresh({
+            place = { "statusline" },
+        })
+    end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+    callback = function()
+        -- This is going to seem really weird!
+        -- Instead of just calling refresh we need to wait a moment because of the nature of
+        -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+        -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+        -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+        -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+        local timer = vim.loop.new_timer()
+        timer:start(
+            50,
+            0,
+            vim.schedule_wrap(function()
+                lualine.refresh({
+                    place = { "statusline" },
+                })
+            end)
+        )
+    end,
+})
