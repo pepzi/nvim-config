946c96fd4ab94dc7958f0dbae8ac76953edabacc
Robert Sand <robert@pepzi.org>
2024-03-30 22:55:56 +0100

    add flashing yank/undo/redo

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -178,7 +178,29 @@ local plugins = {
         -- Configuration here, or leave empty to use defaults
       })
     end
-  }
+  },
+  {
+    "tzachar/highlight-undo.nvim",
+    event = "VeryLazy",
+    opts = {
+      hlgroup = "CurSearch",
+      duration = 150,
+      keymaps = {
+        { "n", "u",     "undo", {} }, -- If you remap undo/redo, change this
+        { "n", "<C-r>", "redo", {} },
+      },
+    },
+    config = function(_, opts)
+      require("highlight-undo").setup(opts)
+
+      -- Also flash on yank.
+      vim.api.nvim_create_autocmd("TextYankPost", {
+        desc = "Highlight yanked text",
+        pattern = "*",
+        callback = function() vim.highlight.on_yank() end,
+      })
+    end,
+  },
 }
 
 local opts = {}
