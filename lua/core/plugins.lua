ce354c9d45875d443f54e65be930944bea2ac47e
Robert Sand <robert@pepzi.org>
2024-05-28 00:56:29 +0200

    update neogit, add project.nvim, noice keybinds

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -16,7 +16,12 @@ local plugins = {
 	--  'wbthomason/packer.nvim',
 	-- theme
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	"ahmedkhalf/project.nvim",
+	{
+		"ahmedkhalf/project.nvim",
+		config = function()
+			require("project_nvim").setup({})
+		end,
+	},
 	-- file explorer tree
 	"nvim-tree/nvim-tree.lua",
 	-- filesystem editor
@@ -169,7 +174,76 @@ local plugins = {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
 			-- add any options here
+		},
+		keys = {
+			{
+				"<S-Enter>",
+				function()
+					require("noice").redirect(vim.fn.getcmdline())
+				end,
+				mode = "c",
+				desc = "Redirect Cmdline",
+			},
+			{
+				"<leader>snl",
+				function()
+					require("noice").cmd("last")
+				end,
+				desc = "Noice Last Message",
+			},
+			{
+				"<leader>snh",
+				function()
+					require("noice").cmd("history")
+				end,
+				desc = "Noice History",
+			},
+			{
+				"<leader>sna",
+				function()
+					require("noice").cmd("all")
+				end,
+				desc = "Noice All",
+			},
+			{
+				"<leader>snd",
+				function()
+					require("noice").cmd("dismiss")
+				end,
+				desc = "Dismiss All",
+			},
+			{
+				"<c-f>",
+				function()
+					if not require("noice.lsp").scroll(4) then
+						return "<c-f>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll forward",
+				mode = { "i", "n", "s" },
+			},
+			{
+				"<c-b>",
+				function()
+					if not require("noice.lsp").scroll(-4) then
+						return "<c-b>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll backward",
+				mode = { "i", "n", "s" },
+			},
 		},
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function DoThings(Type $add = null)
-{
-    // code...
-    for ($i = 0; $i < 82; $i++) {
-        print ("$i");
-    }
-}
