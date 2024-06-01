4796b680ffa1649a908f070c379e7c972714684e
Robert Sand <robert@pepzi.org>
2024-06-01 23:58:50 +0200

    fix key-bindings for hapoon, add descriptions to <leader> root

--- lua/core/plugin_config/which-key.lua
+++ lua/core/plugin_config/which-key.lua
@@ -10,7 +10,10 @@ wk.register({
 	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
 	["<leader>d"] = { name = "[D]ap", _ = "which_key_ignore" },
 	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-	["<leader>o"] = { name = "[O]bsidian", _ = "which_key_ignore" },
 	["<leader>i"] = { name = "Code[I]um", _ = "which_key_ignore" },
+	["<leader>l"] = { name = "[L]spsaga", _ = "which_key_ignore" },
+	["<leader>p"] = { name = "[P]roject", _ = "which_key_ignore" },
+	["<leader>n"] = { name = "[N]oice", _ = "which_key_ignore" },
+	-- ["<leader>o"] = { name = "[O]bsidian", _ = "which_key_ignore" },
 	-- ['<leader>ol'] = { name = '[O]bsidian [L]inks', _ = 'which_key_ignore' },
 })
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -193,21 +193,28 @@ local plugins = {
 				desc = "Redirect Cmdline",
 			},
 			{
-				"<leader>snl",
+				"<leader>ne",
+				function()
+					require("noice").cmd("errors")
+				end,
+				desc = "Noice Error Messages",
+			},
+			{
+				"<leader>nl",
 				function()
 					require("noice").cmd("last")
 				end,
 				desc = "Noice Last Message",
 			},
 			{
-				"<leader>snh",
+				"<leader>nh",
 				function()
 					require("noice").cmd("history")
 				end,
 				desc = "Noice History",
 			},
 			{
-				"<leader>sna",
+				"<leader>na",
 				function()
 					require("noice").cmd("all")
 				end,
